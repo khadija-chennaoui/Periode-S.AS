320f4cac30c75c763646b5bd5250401882ed98e1
khadija-chennaoui <khadijach896@gmail.com>
2021-11-24 14:42:06 +0100

    pas complet

--- brief/brief.c
+++ brief/brief.c
@@ -10,29 +10,111 @@ typedef struct {
     double Montant;
 }infoclients ;
 
-  void ajoutercompte(){
-     infoclients info;
-     printf("CIN\n");
-     scanf("%s",info.CIN);
-     printf("Nom\n");
-     scanf("%s",info.NOM);
-     printf("Preno\nm");
-     scanf("%s",info.prenom);
+
+infoclients info[100];
+int i = 0;
+
+void menu ();
+void ajoutercompte(){
+     printf("CIN \n");
+     scanf("%s",info[0].CIN);
+     printf("Nom \n");
+     scanf("%s",info[0].NOM);
+     printf("Prenom \n");
+     scanf("%s",info[0].prenom);
      printf("Montant \n");
-     scanf("%f",&info.Montant); }
+     scanf("%lf",&info[0].Montant); 
+
+     printf("%s  ",info[0].CIN);
+     printf("%s  ",info[0].NOM);
+     printf("%s  ",info[0].prenom);
+     printf("%lf  ",info[0].Montant);
+
+     menu();
+     
+     }
      
       void ajouterpluscompte(){
           int i,nombre;
-          for(i=0;i<nombre;i++){
-          ajoutercompte();
-          return 0;
-          
-          void menu (){
+          printf("combien voulez-vous enregistrer des clients ?");
+          scanf("%d",&nombre);
+             for(i=0;i<nombre;i++){
+                printf("CIN \n");
+                scanf("%s",info[i].CIN);
+                printf("Nom \n");
+                scanf("%s",info[i].NOM);
+                printf("Prenom \n");
+                scanf("%s",info[i].prenom);
+                printf("Montant \n");
+                scanf("%lf",&info[i].Montant);}
+                system("cls");                    
+                menu() ;   
+                }
+int rechercheparcin(){
+    char cin[30] ;
+    printf("entrer le cin :");
+    scanf("%s",cin);
+    for(int i=0; i<100; i++){
+        if(strcmp(info[i].CIN,cin)==0)
+        
+    return i ;
+    
+    }
+}void retrait(){
+        float Montant ;
+        int i = rechercheparcin();
+        printf("\n %s %s %s %f \n",info [i].CIN,info[i].NOM,info[i].prenom,info[i].Montant);
+        printf(" donner le Mantant pour retrait :");
+        scanf("%f",&Montant);
+        if(Montant > info[i].Montant){
+            printf("impossible pour retrait !!!");
+        }
+        else{
+            info[i].Montant = info[i].Montant - Montant;
+            printf("\n%s %s %s %f \n",info[i].CIN,info[i].NOM,info[i].prenom,info[i].Montant);}
+        }
+    
+void depot(){
+        float Montant ;
+        int i = rechercheparcin();
+        printf("\n %s %s %s %f \n",info [i].CIN,info[i].NOM,info[i].prenom,info[i].Montant);
+        printf(" donner le Mantant pour retrait :");
+        scanf("%f",&Montant);
+        
+            info[i].Montant = info[i].Montant + Montant;
+            printf("\n%s %s %s \n",info[i].CIN,info[i].NOM,info[i].prenom,info[i].Montant);
+    }
+
+
+void operation(){
     int choix;
-    printf("**************Bienvenue**************\n");
-    printf("*******MENU*******\n");
-    printf("choisissez le nombre de votre choix\n");
-    scanf("%d",choix);
+    system("cls");
+    printf("\t<<<<<<<<<< les operations >>>>>>>>>> \n\n");
+    printf("\t  __1__ retrait____: \n\n");
+    printf("\t__2__ depot____:\n\n");
+    printf("\tentrer votre choix :");
+    scanf("%d",&choix);
+
+    switch (choix)
+    {
+    case 1 :
+    retrait();
+     break;
+    case 2 :
+    depot();
+    break;
+    default:
+       printf("^^^^erreur de saisi^^^^  ");
+        break;
+    }
+}
+
+void menu (){
+            int choix;
+        printf("\t\t\t**************Bienvenue**************\t\n");
+        printf("\t\t    *******MENU*******\n");
+        printf("\tchoisissez le nombre de votre choix\n");
+        
     do{
         printf("1-introduire un compte bancaire\n");
         printf("2-introduire plusieurs ncompte bancaire\n");
@@ -45,32 +127,32 @@ typedef struct {
     switch(choix)
     {
         case 1 :
-        printf("ajoutercompte");
+       ajoutercompte();
         break;
         case 2 :
-        printf("2") ;
+        ajouterpluscompte();
         break;
         case 3 :
-        printf("3") ;
+        operation();
         break;
         case 4 :
-        printf("3") ;
+        printf("4") ;
         break ;
         case 5 :
-        printf("4"); 
+        printf("5"); 
         break ; 
         default :
-        printf("5"); 
+        printf("quitter l'application");
         break;
+    
+        
     }
-}while (choix != 6);
+}while(choix<1 || choix>6);
 
 }
 int main ()
 {
-menu () ;
-
+    menu();
+   
 return 0 ;
-
-
 }
